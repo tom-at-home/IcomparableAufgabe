8b88649750d972f1301782f01b2582dd3254156d
tom <tomaszu@C-503-R2G1>
2017-04-10 13:29:37 +0200

    Aufgabe 1 fertig

---
+++ IcomparableAufgabe/House.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcomparableAufgabe
+{
+    class House : IComparable<House>
+    {
+
+        private int height;
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public int CompareTo(House other)
+        {
+            if (this.Height > other.Height)
+            {
+                return 1;
+            }
+            else if (this.Height < other.Height)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
--- IcomparableAufgabe/Program.cs
+++ IcomparableAufgabe/Program.cs
@@ -8,8 +8,45 @@ namespace IcomparableAufgabe
 {
     class Program
     {
+
+        List<House> houses = new List<House>();
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Init();
         }
+
+        public void Init()
+        {
+            
+
+            House h1 = new House();
+            h1.Height = 200;
+
+            House h2 = new House();
+            h2.Height = 180;
+
+            House h3 = new House();
+            h3.Height = 220;
+
+            houses.Add(h1);
+            houses.Add(h2);
+            houses.Add(h3);
+
+            showHouses();
+            houses.Sort();
+            showHouses();
+
+        }
+
+        public void showHouses()
+        {
+            foreach (House item in houses)
+            {
+                Console.WriteLine(item.Height);
+            }
+        }
+
     }
 }
